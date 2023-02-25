de6e98c3253e1392f7e4ec701ea5ba82f0278407
Pastir Ivan <ivan@pastir.me>
2023-02-25 12:51:53 +0200

    Test

--- services.go
+++ services.go
@@ -3,17 +3,30 @@ package ManageServer
 type (
 	// Service - service  control
 	Service interface {
-		// Init - init service initialization => configuration, connections...
+		// Init service initialization => configuration, connections...
 		Init() error
 
-		// Close - close connections, release resources
+		// Close connections, release resources
 		Close() error
 
-		// Check - service availability check
+		// Check service availability check
 		Check() error
+
+		Name() string
 	}
 
 	ServiceWatcher struct {
 		Services []Service
 	}
 )
+
+func (sw *ServiceWatcher) InitServices() error {
+
+	for _, serv := range sw.Services {
+		err := serv.Init()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
